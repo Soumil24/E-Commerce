cbd2b28f86c616189da454ac40df5a58b5f2bdf4
Soumil24 <soumilking45@gmail.com>
2025-01-03 15:23:26 +0530

    nav

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -4,8 +4,11 @@ import myContext from '../../context/data/Mycontext'
 import { use } from 'react'
 import { addtocart } from '../../redux/cartSlice'
 import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 function ProductCard() {
+
+    const navigate = useNavigate();
     const context = useContext(myContext)
     const { mode, product, searchkey, filterType, filterPrice} = context
 
@@ -34,7 +37,7 @@ function ProductCard() {
                         return (
                             <div key={index} className="p-4 md:w-1/4  drop-shadow-lg " >
                                 <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
-                                    <div onClick={()=> navigate(`/login`)} className="flex justify-center cursor-pointer" >
+                                    <div onClick={()=> navigate(`productinfo/${item.id}`)} className="flex justify-center cursor-pointer" >
                                         <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src={imageUrl} />
                                     </div>
                                     <div className="p-5 border-t-2">
