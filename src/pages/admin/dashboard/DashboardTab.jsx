fe5f6d99890f8041e3d171ca1d384ff2a9898474
Soumil24 <soumilking45@gmail.com>
2025-01-03 16:04:53 +0530

    add pro fix

--- src/pages/admin/dashboard/DashboardTab.jsx
+++ src/pages/admin/dashboard/DashboardTab.jsx
@@ -4,10 +4,11 @@ import myContext from '../../../context/data/Mycontext';
 import { MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { FaUser, FaCartPlus } from 'react-icons/fa';
 import { AiFillShopping, AiFillPlusCircle, AiFillDelete } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function DashboardTab() {
     const context = useContext(myContext)
+    const navigate = useNavigate()
     const { mode, product, edithandle, deleteProduct, order, user } = context
     let [isOpen, setIsOpen] = useState(false)
 
@@ -22,7 +23,8 @@ function DashboardTab() {
     }
 
     const AddProductBtn = () => {
-        window.location.href = '/addproduct'
+        //window.location.href = '/addproduct'
+        navigate(`/addproduct`)
     }
     return (
         <>
