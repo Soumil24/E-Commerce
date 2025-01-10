5097fc0813ec1064a4034e450a444a5d842e747a
Soumil24 <soumilking45@gmail.com>
2025-01-10 18:30:34 +0530

    test2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -31,7 +31,8 @@ function Home() {
         <Filter/>
         <ProductCard/>
         <div className= ' w-screen flex items-center justify-center'>
-        <button className='px-2 py-1 rounded-xl font-medium text-base bg-gray-300' onClick={()=> navigate("/allproducts")}>View More</button>
+        <button className='px-2 py-1 rounded-xl font-medium text-base bg-gray-300' onClick={()=> navigate("/allproducts")}> View More </button>
+        
         </div>
         <Track/>
         <Testimonial/>
